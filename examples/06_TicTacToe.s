07260b0b0c454805ed5a95068ffc141e4a38b783
lross2k <luisross2000@outlook.com>
2023-06-11 10:26:33 -0600

    I tried to capture inputs
    Well, apparently the small wrapper.s is too high level for my
    purposes, so I started learning iNES format, and now I'm going
    down the rabbit hole of NES programming
    
    ~lorss2k~

--- examples/06_TicTacToe.s
+++ examples/06_TicTacToe.s
@@ -1,9 +1,11 @@
 ; ======== Game board ========
 ; Consider the memory to be visualized as follows
-;     00 01 02
-; 00  __|__|__ 
-; 10  __|__|__ 
-; 20    |  |   
+;  00 01 02
+;  __|__|__ 
+;  03 04 05
+;  __|__|__ 
+;  06 07 08
+;    |  |   
 ; Where we will consider values as
 ; $00 is empty
 ; $01 is X
@@ -20,6 +22,33 @@
 .segment "CODE"
 
 .proc Main
+io_loop:
+     ; Read controller 1 to make decisions
+     lda $4016
+     and #%10000000 ; Mask button A
+     sta $00
+     lda $4016
+     and #%01000000 ; Mask button B
+     sta $01
+     lda $4016
+     and #%00100000 ; Mask button SELECT
+     sta $02
+     lda $4016
+     and #%00010000 ; Mask button START
+     sta $03
+     lda $4016
+     and #%00001000 ; Mask button UP
+     sta $04
+     lda $4016
+     and #%00000100 ; Mask button DOWN
+     sta $05
+     lda $4016
+     and #%00000010 ; Mask button LEFT
+     sta $06
+     lda $4016
+     and #%00000001 ; Mask button RIGHT
+     sta $07
+     jmp io_loop
     
 .endproc
 
